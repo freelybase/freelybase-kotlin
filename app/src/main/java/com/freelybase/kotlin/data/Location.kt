effb8059a96f569cb0566bd0ddb42a9ea8df91b8
大菠萝 <daboluo719@gmail.com>
2026-05-14 23:57:07 +0800

    feat:1.0.8

--- app/src/main/java/com/freelybase/kotlin/data/Location.kt
+++
@@ -1,8 +0,0 @@
-package com.freelybase.kotlin.data
-
-/** Object 类型字段：嵌套对象，直接用 data class */
-data class Location(
-    val city: String = "",
-    val lat: Double = 0.0,
-    val lng: Double = 0.0,
-)
